fbadde6cd689b54d5982d7efe580276c692a0d18
daren-scerri <daren.scerri@mcast.edu.mt>
2019-10-13 20:50:52 +0200

    commit

---
+++ Fun with Shapes/Assets/Scripts/Chessboard.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chessboard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject myprefab= Resources.Load("Prefabs/Square") as GameObject;
+        for (int r=0;r<4;r++)
+		{
+			for (int c=0;c<4;c++)
+			{
+				
+				GameObject t = Instantiate(myprefab,new Vector3(c,r),Quaternion.identity);
+				
+				if (r%2==0){
+					if (c%2 == 0)
+					{
+						t.GetComponent<SpriteRenderer>().color = Color.black;
+					}
+				}else {
+					if (c%2 != 0)
+					{
+						t.GetComponent<SpriteRenderer>().color = Color.black;
+					}				
+				}
+				
+				
+				
+			
+			}
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Fun with Shapes/Assets/Scripts/MoveWithKeys.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWithKeys : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject myPrefab, myShape;
+    void Start()
+    {
+        myPrefab=Resources.Load("Prefabs/Circle") as GameObject;
+        myShape=Instantiate(myPrefab, new Vector3(0,0,0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow)) 
+        {
+            Debug.Log("key pressed");
+            myShape.transform.position = new Vector3(myShape.transform.position.x+1, myShape.transform.position.y,0) ;
+        }
+    }
+
+}
